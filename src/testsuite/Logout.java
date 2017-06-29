7e2b38dbe5469476ecbeda7a8dd25be4bc58bc5a
sai krishna <selenium.saikrishna@gmail.com>
2017-06-29 04:35:32 -0700

    Third  Commit

--- src/testsuite/Logout.java
+++ src/testsuite/Logout.java
@@ -9,6 +9,8 @@ public class Logout  extends BaseClass{
 	@Test
 	public void logoutTest()
 	{
+
+		system.out.println("Logging out")
 		driver.findElement(By.id("welcome")).click();
 		driver.findElement(By.linkText("Logout")).click();
 	}
